30315bfd859acaf71152a75fb81e7afa87d2113e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2024-09-19 07:42:36 +0600

    this is first

--- js/login.js
+++ js/login.js
@@ -6,8 +6,18 @@ document.getElementById('login-btn').addEventListener('click', function(event){
   // step-2: pervent default behavior(prevent reloadign browser)
   event.preventDefault();  // Vejal to beginners
   console.log( 'login button form ');
-  // step-3: get the phone number
+  // step-3: get the phone number & pinnumber
   const phonenumber = document.getElementById('phone-number').value;
-  console.log( phonenumber);
+  const pinnmber = document.getElementById('pin-number').value;
+
+  console.log( phonenumber, pinnmber);
+  // step-4: validate phone and pin
+  if(phonenumber === '8' && pinnmber === '1234'){
+    console.log( 'Logged in');
+    window.location.href ='/home.html';
+    // step-5: allow user to use the website
+  }else{
+    alert('Please check again');
+  }
 
 })

42ee956f1d30d5c3a0fe25b7adf974270d6edf52
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2024-09-19 08:18:51 +0600

    cash out form

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
-console.log( 'home money');
+// console.log( 'home money');
+/**
+ * 
+ */
+
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+  //  pervent page reload after form submit
+  event.preventDefault();
+
+    // step-2: get money to be added to the account
+    const addMoneyInput =  document.getElementById('input-add-money').value;
+    console.log( addMoneyInput);
+    // get the pin number provided
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log( inputPinNumber);
+})
